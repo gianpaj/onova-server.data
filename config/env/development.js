3d362e8fc2d59c4065ca0668436dad9fb4c6afe8
Kunal Kapadia <kunalkapadia12@gmail.com>
2017-02-20 20:26:05 +0530

    Use dotenv for configuration management (#302)

--- config/env/development.js
+++
@@ -1,9 +0,0 @@
-const devConfig = {
-  env: 'development',
-  MONGOOSE_DEBUG: true,
-  jwtSecret: '***REMOVED***',
-  db: 'mongodb://localhost/express-mongoose-es6-rest-api-development',
-  port: 4040
-};
-
-export default devConfig;
--- config/env/production.js
+++
@@ -1,8 +0,0 @@
-const prodConfig = {
-  env: 'production',
-  jwtSecret: '***REMOVED***',
-  db: 'mongodb://localhost/express-mongoose-es6-rest-api-production',
-  port: 4040
-};
-
-export default prodConfig;
