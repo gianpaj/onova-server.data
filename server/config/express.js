99b80f6f2c18dddf44039cb70742d8527acc2ebf
Gianfranco Palumbo <gianpa@gmail.com>
2018-04-16 20:43:14 +0300

    tools: enable detailed API console logging in dev env

--- server/config/express.js
+++ server/config/express.js
@@ -72,7 +72,20 @@ app.set('view engine', 'pug');
 // tell Express to use the remote IP address
 app.set('trust proxy', true);
 
-// enable detailed API console logging
+// enable detailed API console logging in dev env
+if (config.env === 'development') {
+  expressWinston.requestWhitelist.push('body');
+  expressWinston.responseWhitelist.push('body');
+  app.use(
+    expressWinston.logger({
+      winstonInstance,
+      meta: true, // optional: log meta data about request (defaults to true)
+      msg:
+        'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
+      colorize: true, // Color the status code (default green, 3XX cyan, 4XX yellow, 5XX red).
+    })
+  );
+}
 if (config.env === 'test') {
   expressWinston.requestWhitelist.push('body');
   expressWinston.responseWhitelist.push('body');
