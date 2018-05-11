b8fda710e9ba10c0b2f3e21a1c2547272f73df80
Gianfranco Palumbo <gianpa@gmail.com>
2018-05-11 13:27:12 +0300

    refactor(models): create index file that exports all models

---
+++ server/models/like.model.js
@@ -0,0 +1,47 @@
+// @flow
+
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+var LikeSchema = new Schema({
+  product: {
+    type: Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+});
+
+export class LikeDoc /*:: extends Mongoose$Document */ {
+  product: MongoId;
+  userId: MongoId;
+  dateCreated: Date;
+}
+
+LikeSchema.loadClass(LikeDoc);
+
+// Never return '__v' fields in the JSON representation
+// Note that this doesn't effect `toObject`
+OrderSchema.set('toJSON', {
+  getters: true,
+  transform: (doc, ret) => {
+    delete ret.id;
+    delete ret._id;
+    delete ret.__v;
+    return ret;
+  },
+});
+
+LikeSchema.index({ product: 1, dateCreated: 1 });
+LikeSchema.index({ userId: 1, dateCreated: 1 });
+
+export default mongoose.model('Like', LikeSchema);
