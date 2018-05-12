3c2a418a1677a36b37bc9cc4c3f6e5aed6c7dbc8
Gianfranco Palumbo <gianpa@gmail.com>
2018-05-12 18:55:01 +0300

    Orders: improve tests and don't filter reviews twice

--- server/__tests__/order.test.js
+++ server/__tests__/order.test.js
@@ -418,7 +418,15 @@ describe('## Order APIs', () => {
         .then(res => {
           const o = res.body.data;
           expect(Array.isArray(o));
-          expect(o.length).toBe(3);
+          expect(o.length).toBe(3); // FIXME: use counters
+          expect(Object.keys(o[0]).sort()).toEqual(orderFields.sort());
+          expect(Object.keys(o[0].buyer).sort()).toEqual(
+            ['_id', 'accountStatus', 'id', 'profilePic', 'username'].sort()
+          );
+          // this user didn't upload the profilePic
+          expect(Object.keys(o[0].seller).sort()).toEqual(
+            ['_id', 'accountStatus', 'id', 'username'].sort()
+          );
         });
     });
 
@@ -431,7 +439,7 @@ describe('## Order APIs', () => {
           const o = res.body.data;
           expect(Array.isArray(o));
           expect(Object.keys(o[0]).sort()).toEqual(orderFields.sort());
-          expect(o.length).toBe(4);
+          expect(o.length).toBe(4); // FIXME: use counters
         });
     });
 
@@ -443,10 +451,7 @@ describe('## Order APIs', () => {
         .then(res => {
           const o = res.body.data;
           expect(Array.isArray(o));
-          expect(Object.keys(o[0]).sort()).toEqual(orderFields.sort());
-          expect(Object.keys(o[0].buyer).sort()).toEqual(
-            ['_id', 'accountStatus', 'id', 'profilePic', 'username'].sort()
-          );
+          expect(o.length).toBe(4); // FIXME: use counters
         });
     });
   });
