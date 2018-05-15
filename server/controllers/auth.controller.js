db834ed3829c48ae758689dda661a317d1214ff3
Gianfranco Palumbo <gianpa@gmail.com>
2018-05-15 13:00:48 +0300

    ukrainian activation site

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -99,10 +99,9 @@ function activate(req, res) {
         data.paragraph =
           'There was something wrong with the link you received. Note that it expires after 24 hours. Please request a new one from the App or email <a href="mailto:hello@onova.co">hello@onova.co</a> for support.';
       } else if (verDoc.user.accountStatus == 'notverified') {
-        const { displayName, username } = verDoc.user;
-        data.heading = 'Account activated!';
-        data.paragraph = `Hi five ${username ||
-          displayName}! Your account is now activated (${
+        const { username } = verDoc.user;
+        data.heading = 'Профіль активовано!';
+        data.paragraph = `${username}, Можеш користуватись додатком на повну (${
           verDoc.user.emailAddress
         }).`;
 
@@ -112,10 +111,9 @@ function activate(req, res) {
 
         verDoc.remove();
       } else if (verDoc.user.accountStatus == 'verified') {
-        const { displayName, username } = verDoc.user;
+        const { username } = verDoc.user;
         (data.heading = 'Account is already activated!'),
-          (data.paragraph = `Double hi five ${username ||
-            displayName}! Your account is already activated (${
+          (data.paragraph = `Double hi five ${username}! Your account is already activated (${
             verDoc.user.emailAddress
           }).`);
       }
