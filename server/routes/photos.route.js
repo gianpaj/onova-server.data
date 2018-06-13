40dc764a46fec73d24350e8cde529f2ba1dac998
Gianfranco Palumbo <gianpa@gmail.com>
2018-06-13 12:16:48 +0300

    entropy cropping

--- server/routes/photos.route.js
+++ server/routes/photos.route.js
@@ -6,9 +6,6 @@ import httpStatus from 'http-status';
 import multer from 'multer';
 import gcsSharp from 'multer-sharp';
 const debug = require('debug')('express-mongoose-es6-rest-api:index');
-// import path from 'path';
-
-// import config from '../config/config';
 
 const requireAuth = passport.authenticate('jwt', { session: false });
 const router = express.Router();
@@ -38,7 +35,7 @@ const storage = gcsSharp({
       height: MAX_HEIGHT,
     },
   ],
-  crop: 'center',
+  crop: 16, // sharp.strategy.entropy
   toFormat: 'jpeg',
   // withoutEnlargement: true,
 });
