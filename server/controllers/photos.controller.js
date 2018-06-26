a8e2080e65b73e66ac491332de3a8fa953674c4d
Gianfranco Palumbo <gianpa@gmail.com>
2018-06-26 15:41:07 +0100

    fix prev commit

--- server/controllers/photos.controller.js
+++ server/controllers/photos.controller.js
@@ -1,7 +1,13 @@
 // @flow
 
 import gcsSharp from 'multer-sharp';
+import httpStatus from 'http-status';
 import multer from 'multer';
+import fetch from 'node-fetch';
+import FormData from 'form-data';
+
+const debug = require('debug')('express-mongoose-es6-rest-api:index');
+const download = require('image-downloader');
 
 const MAX_WIDTH = 1440;
 const MAX_HEIGHT = 1440;
@@ -63,7 +69,67 @@ const storageForChatImages = gcsSharp({
 });
 const uploadChatImage = multer({ storage: storageForChatImages });
 
+/**
+ * Upload URL image to VK
+ *
+ * POST /api/photos/upload-to-vk
+ *
+ * @property {*} req - Express request
+ * @property {*} req.body - Express body parameters
+ * @property {string} req.body.upload_url
+ * @property {Array<string>|string} req.body.photos
+ */
+async function uploadToVK(
+  req: express$Request,
+  res: express$Response,
+  next: express$NextFunction
+) {
+  // TODO: check if we have access to VK.com
+
+  // $FlowFixMe
+  const { upload_url, photos } = req.body;
+
+  // download the photos
+  const dest = '/tmp';
+
+  try {
+    const downloads = photos.map(url => download.image({ url, dest }));
+
+    const files = await Promise.all(downloads);
+
+    debug('File(s) saved to', files.map(f => f.filename));
+
+    let uploads = files.map(file => {
+      return new Promise((resolve, reject) => {
+        let formData = new FormData();
+        const filename = file.filename.split('/')[
+          file.filename.split('/').length - 1
+        ];
+        formData.append('photo', file.image, { filename });
+        fetch(upload_url, {
+          method: 'POST',
+          // timeout: ,
+          body: formData,
+        })
+          .then(res => res.json())
+          .then(res => resolve(res))
+          .catch(e => reject(e));
+      });
+    });
+
+    const data = await Promise.all(uploads);
+
+    debug('photo(s) uploaded to VK');
+
+    res.status(httpStatus.CREATED).json({ data });
+  } catch (err) {
+    console.error(err);
+    return next(err);
+  }
+}
+
 export default {
   uploadProductImage,
   uploadChatImage,
+  uploadToVK,
 };
