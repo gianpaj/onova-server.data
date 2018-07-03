d88f6b4ab0f3224c0cbbf0a19ae8e1282414b933
Gianfranco Palumbo <gianpa@gmail.com>
2018-07-03 22:50:07 +0100

    Schedule: accept location coordinates

--- server/config/validation/schedule.validation.js
+++ server/config/validation/schedule.validation.js
@@ -68,6 +68,12 @@ export default {
       //   .items(Joi.string().valid(['fb', 'vk']))
       //   .single()
       //   .required(),
+      latitude: Joi.number()
+        .min(-90)
+        .max(90),
+      longitude: Joi.number()
+        .min(-180)
+        .max(180),
     },
   },
 };
