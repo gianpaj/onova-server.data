10bb939be2316afd9264b97ae0ad563edd47d9c8
Gianfranco Palumbo <gianpa@gmail.com>
2018-07-24 12:23:50 +0300

    Products: return displayName of seller

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -12,7 +12,8 @@ import config from '../config/config';
 
 const Schema = mongoose.Schema;
 
-export const userPopulateFields = 'username accountStatus profilePic';
+export const userPopulateFields =
+  'username accountStatus profilePic displayName';
 
 /**
  * User Schema
