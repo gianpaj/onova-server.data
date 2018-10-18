52b099c1658d0c8e9b53208510d8edca04928292
Gianfranco Palumbo <gianpa@gmail.com>
2018-10-18 12:29:34 +0300

    Shipping Costs: add route and validation

--- server/config/validation/shipping.validation.js
+++ server/config/validation/shipping.validation.js
@@ -3,6 +3,17 @@ import Joi from 'joi';
 export default {
   // GET /api/shipping/departments/${city}
   departments: {
-    city: Joi.string(), // TODO: Validate cities
+    params: {
+      city: Joi.string(), // TODO: validate it's a UUID (8d5a980d-391c-11dd-90d9-001a92567626)
+    },
+  },
+  // GET /api/shipping/costs
+  costs: {
+    query: {
+      price: Joi.string().required(),
+      weight: Joi.number().required(),
+      senderOfficeID: Joi.string().required(),
+      recipientOfficeID: Joi.string().required(),
+    },
   },
 };
