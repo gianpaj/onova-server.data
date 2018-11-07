c159cef7b468f16eb971f9cb0ddc72e3ada09aa7
Gianfranco Palumbo <gianpa@gmail.com>
2018-11-07 16:18:43 +0200

    fix tests

--- server/__tests__/notification.test.js
+++ server/__tests__/notification.test.js
@@ -11,6 +11,7 @@ import config from '../config/config';
 
 import app from '../index';
 
+import { i18n } from '../controllers/order.controller';
 import Order from '../models/order.model';
 import Tag from '../models/tag.model';
 import {
@@ -25,6 +26,8 @@ import { buyerPaidDeal } from '../helpers/shipping';
 
 // This sets the mock adapter on the default instance
 const mock = new MockAdapter(axios);
+
+jest.setTimeout(10000);
 
 /**
  * root level hooks
@@ -441,7 +444,7 @@ describe('## Notification APIs', () => {
                 expect(data.targetUser.toString()).toBe(userId);
                 expect(data.triggeredBy.toString()).toBe(orderId);
                 expect(data.triggeredType).toBe('Order');
-                expect(data.message).toContain('You have a new purchase!');
+                expect(data.message).toContain(i18n.orderPaid);
                 expect(typeof data.random).toBe('string');
                 done();
               });
@@ -457,7 +460,7 @@ describe('## Notification APIs', () => {
           .then(res => {
             const { data } = res.body;
             expect(data[0].triggeredBy.id).toBe(orderId);
-            expect(data[0].notifI18n).toContain('You have a new purchase!');
+            expect(data[0].notifI18n).toContain(i18n.orderPaid);
             expect(data).toHaveLength(numberOfNotifForFirstUser);
           });
       });
@@ -487,9 +490,11 @@ describe('## Notification APIs', () => {
             .expect(httpStatus.OK)
             .then(res => {
               const { data } = res.body;
-              expect(data[0].triggeredBy.id).toBe(orderId);
-              expect(data[0].notifI18n).toContain('You have a new purchase!');
-              expect(data).toHaveLength(numberOfNotifForFirstUser);
+              const notifs = data.filter(
+                n => n.notifI18n !== i18n.orderPaidReminder
+              );
+              expect(notifs[0].notifI18n).toContain(i18n.orderPaid);
+              expect(notifs).toHaveLength(numberOfNotifForFirstUser);
               done();
             });
         }, 500);
@@ -586,7 +591,7 @@ describe('## Notification APIs', () => {
           );
           expect(comment).toBeUndefined();
           expect(data[0].data.text).not.toContain('oops thats');
-          expect(data).toHaveLength(numberOfNotifForFirstUser);
+          expect(data).toHaveLength(numberOfNotifForFirstUser + 1);
         });
     });
   });
