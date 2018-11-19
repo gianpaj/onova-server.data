7617c4bf33009b94d17b17c4ab96bae480176139
Gianfranco Palumbo <gianpa@gmail.com>
2018-11-19 12:50:31 +0200

    style: Models and imports

--- server/models/index.js
+++ server/models/index.js
@@ -1,15 +1,17 @@
-import Block from './block.model';
-import DefaultFollow from './defaultFollow.model';
-import Follow from './follow.model';
+import Block, { BlockDoc } from './block.model';
+import DefaultFollow, { DefaultFollowDoc } from './defaultFollow.model';
+import Cities from './cities.model';
+import Deparments from './departments.model';
+import Follow, { FollowDoc } from './follow.model';
 // import Like from './like.model';
-import Notification from './notification.model';
-import Order from './order.model';
-import Product from './product.model';
-import Report from './report.model';
-import Review from './review.model';
-import Tag from './tag.model';
-import User from './user.model';
-import Verification from './verification.model';
+import Notification, { NotificationDoc } from './notification.model';
+import Order, { OrderDoc } from './order.model';
+import Product, { ProductDoc, CommentDoc } from './product.model';
+import Report, { ReportDoc } from './report.model';
+import Review, { ReviewDoc } from './review.model';
+import Tag, { TagDoc } from './tag.model';
+import User, { UserDoc } from './user.model';
+import Verification, { VerificationDoc } from './verification.model';
 
 Block.syncIndexes();
 DefaultFollow.syncIndexes();
@@ -24,15 +26,29 @@ Verification.syncIndexes();
 
 export {
   Block,
+  BlockDoc,
+  Cities,
+  CommentDoc,
+  Deparments,
   DefaultFollow,
+  DefaultFollowDoc,
   Follow,
+  FollowDoc,
   // Like,
   Notification,
+  NotificationDoc,
   Order,
+  OrderDoc,
   Product,
+  ProductDoc,
   Report,
+  ReportDoc,
   Review,
+  ReviewDoc,
   Tag,
+  TagDoc,
   User,
+  UserDoc,
   Verification,
+  VerificationDoc,
 };
