f1aada57449a1133712b94ca641396bdf344bc2e
Gianfranco Palumbo <gianpa@gmail.com>
2018-11-20 10:28:28 +0200

    fix price update

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -408,7 +408,7 @@ function update(
 ) {
   const { body } = req;
 
-  if (parseFloat(body.price) <= minPrice) {
+  if (parseFloat(body.price) < minPrice) {
     const APIerr = new APIError(
       `Invalid product price. The minimum price is ${minPrice} UAH`,
       httpStatus.BAD_REQUEST
