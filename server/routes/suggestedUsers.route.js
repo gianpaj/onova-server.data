146e012daf215fc0a291ea4de0bdd9d0ac93125d
Gianfranco Palumbo <gianpa@gmail.com>
2018-12-03 11:44:07 +0200

    first test

---
+++ server/routes/suggestedUsers.route.js
@@ -0,0 +1,27 @@
+// @flow
+import express from 'express';
+import validate from 'express-validation';
+import passport from 'passport';
+
+import paramValidation from '../config/validation/comment.validation';
+import suggestedUsersCtrl from '../controllers/suggestedUsers.controller';
+
+// loads Authenticated user document in `req.user`
+const requireAuth = passport.authenticate('jwt', { session: false });
+
+const router = express.Router();
+
+router
+  .route('/')
+
+  // GET /api/suggested-users - List my suggested users
+  .get(requireAuth, suggestedUsersCtrl.list);
+
+// DELETE /api/suggested-users/:uuid - Discard a suggested user
+// .delete(
+//   validate(paramValidation.discard),
+//   requireAuth,
+//   suggestedUsersCtrl.discard
+// );
+
+export default router;
