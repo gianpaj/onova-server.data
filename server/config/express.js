894a1253b350df4505e73960ee3209d6150c5da5
Gianfranco Palumbo <gianpa@gmail.com>
2018-12-20 12:10:41 +0000

    rotate log files every day, not every hour. and move logs to their own directory

--- server/config/express.js
+++ server/config/express.js
@@ -127,6 +127,7 @@ if (config.env === 'production') {
       transports: [
         new winston.transports.DailyRotateFile({
           filename: 'access-%DATE%.log',
+          dirname: './logs',
           datePattern: 'YYYY-MM-DD',
           zippedArchive: true,
           maxSize: '20m',
@@ -176,7 +177,7 @@ if (config.env === 'development') {
       transports: [
         new winston.transports.DailyRotateFile({
           filename: 'error-%DATE%.log',
-          datePattern: 'YYYY-MM-DD-HH',
+          datePattern: 'YYYY-MM-DD',
           zippedArchive: true,
           maxSize: '20m',
           maxFiles: '14d',
