3cb46078a3af48f05156fd92b24377aa9c910b3e
Gianfranco Palumbo <gianpa@gmail.com>
2018-12-21 11:52:13 +0000

    rename /api/v2/drop to /api/v2/drops

--- server/routes/drop.route.js
+++ server/routes/drop.route.js
@@ -13,7 +13,7 @@ const router = express.Router();
 router
   .route('/')
 
-  // POST /api/drop - create a drop
+  // POST /api/v2/drops - create a drop
   .post(validate(paramValidation.createDrop), requireAuth, dropCtrl.create);
 
 export default router;
