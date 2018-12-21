0f94b6e017103c384ff99430d77313690143e2f2
Gianfranco Palumbo <gianpa@gmail.com>
2018-12-21 16:23:27 +0000

    feat+test(Drops): add location to items in drops

--- server/config/validation/drop.validation.js
+++ server/config/validation/drop.validation.js
@@ -44,10 +44,12 @@ export default {
       // description: validation.description.required(),
       latitude: Joi.number()
         .min(-90)
-        .max(90),
+        .max(90)
+        .required(),
       longitude: Joi.number()
         .min(-180)
-        .max(180),
+        .max(180)
+        .required(),
     },
   },
 };
