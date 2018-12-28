50d831b2c6f0230e7f1863db6efc46dfdbb75a6f
Gianfranco Palumbo <gianpa@gmail.com>
2018-12-28 17:18:55 +0200

    Drop route: don't require auth for listings

--- server/routes/drop.route.js
+++ server/routes/drop.route.js
@@ -13,7 +13,7 @@ const router = express.Router();
 router
   .route('/')
   // GET /api/v2/drops - get a user's drops
-  .get(validate(paramValidation.getDrops), requireAuth, dropCtrl.get)
+  .get(validate(paramValidation.getDrops), dropCtrl.get)
 
   // POST /api/v2/drops - create a drop
   .post(validate(paramValidation.createDrop), requireAuth, dropCtrl.create);
