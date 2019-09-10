47eebb088033f093937a8189716ff2386dd7df4b
Gianfranco Palumbo <gianpa@gmail.com>
2019-09-10 23:04:29 +0300

    fix

--- server/runners/instagram.runner.js
+++ server/runners/instagram.runner.js
@@ -81,12 +81,10 @@ export default class InstagramRunner {
       const IG_usernames = users.map(u => u.scraping.instagram);
       // Chunk up the scraping of instagram users to 4 at the same time
       let user_pages = await Promise.all(
-        IG_usernames.map(
-          throat(4, instagramScraping.scrapeUserPageDeep).catch(e => {
-            console.error(e);
-            return e;
-          })
-        )
+        IG_usernames.map(throat(4, instagramScraping.scrapeUserPageDeep)).catch(e => {
+          console.error(e);
+          return e;
+        })
       );
 
       user_pages = user_pages.filter(user_page => !(user_page instanceof Error));
