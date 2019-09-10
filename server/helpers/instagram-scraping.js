57294b137f8e22ed045c5129b6df91953d5d751a
Gianfranco Palumbo <gianpa@gmail.com>
2019-09-10 23:58:05 +0300

    catch promise

--- server/helpers/instagram-scraping.js
+++ server/helpers/instagram-scraping.js
@@ -476,6 +476,9 @@ exports.scrapeUserPageDeep = function(username) {
         reject(new Error('Error scraping user page "' + username + '"'));
       }
     });
+  }).catch(e => {
+    console.error(e);
+    return e;
   });
 };
 
