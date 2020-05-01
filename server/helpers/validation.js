0746932484a0f4390a1e80ce63ca95344bdf4ec6
Gianfranco Palumbo <gianpa@gmail.com>
2020-05-01 11:37:06 +0100

    1. change minimum required weight from 100 to 50gr 2. change minimum required price from 50 to 10

--- server/helpers/validation.js
+++ server/helpers/validation.js
@@ -29,7 +29,9 @@ export default {
     .single()
     .error(() => ({ message: 'Invalid product tag' })),
   uuid: Joi.string().regex(shortid),
-  weight: Joi.number().min(100).max(5000),
+  weight: Joi.number()
+    .min(50)
+    .max(5000),
   shortid,
   username: Joi.string()
     .regex(username)
